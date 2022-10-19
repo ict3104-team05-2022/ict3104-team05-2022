cb267ec668df840ab5e2eddf6ce7dc80eedaac8e
Johanna Jimeno <johannajimenoo@gmail.com>
2022-10-20 01:03:40 +0800

    T05-167 Organised the RGB .npy files into folders

--- i3d-feature-extraction/main.py
+++ i3d-feature-extraction/main.py
@@ -1,14 +1,18 @@
-import numpy as np
 from omegaconf import OmegaConf
 from tqdm import tqdm
 
 from utils2.utils import build_cfg_path, form_list_from_user_input, sanity_check
 
+import shutil
+import os
+import os.path
+
 
 def main(args_cli):
     # config
     args_yml = OmegaConf.load(build_cfg_path(args_cli.feature_type))
-    args = OmegaConf.merge(args_yml, args_cli)  # the latter arguments are prioritized
+    # the latter arguments are prioritized
+    args = OmegaConf.merge(args_yml, args_cli)
     # OmegaConf.set_readonly(args, True)
     sanity_check(args)
 
@@ -36,34 +40,52 @@ def main(args_cli):
     elif args.feature_type == 'clip':
         from models2.clip.extract_clip import ExtractCLIP as Extractor
     else:
-        raise NotImplementedError(f'Extractor {args.feature_type} is not implemented.')
+        raise NotImplementedError(
+            f'Extractor {args.feature_type} is not implemented.')
 
     extractor = Extractor(args)
 
     # unifies whatever a user specified as paths into a list of paths
-    video_paths = form_list_from_user_input(args.video_paths, args.file_with_video_paths, to_shuffle=True)
+    video_paths = form_list_from_user_input(
+        args.video_paths, args.file_with_video_paths, to_shuffle=True)
 
     print(f'The number of specified videos: {len(video_paths)}')
-    print(f"Extracting features from video....")
 
     for video_path in tqdm(video_paths):
-        # print(f'Extracting for {video_path}')
-        # Get the video file name from the video path
-        video_file_name = video_path.split('/')[-1]
-        video_file_name = video_file_name.split('.')[0]
-        # print(f'Video File Name {video_file_name}')
-        # extractor._extract(video_path) # note the `_` in the method name
-        feature_dict = extractor.extract(video_path)
-        # print(f'Feature Dict for {feature_dict}')
-        rgb = list(feature_dict.items())[0]
-        # print(f'RGB 0 {rgb[0]}')
-        # print(f'RGB 1 {rgb[1]}')
-        # Store it in dictionary
-        rgb_dict = {rgb[0]: rgb[1]}
-        # print(f'RGB Dict {rgb_dict}')
-        data = np.expand_dims(rgb[1], axis=(2, 1)) # Reshape the data to fit into the TSU model
-        # print(f'Data {data}')
-        np.save("./output/RGB_TEST/" + video_file_name + ".npy", data)
+        extractor._extract(video_path)  # note the `_` in the method name
+
+    # yep, it is this simple!
+
+    source_path = './output/RGB_TEST'
+    files = os.listdir(source_path)
+
+    for file in files:
+        filename = file[:-4]
+        filepath = os.path.join(source_path, file)
+        type = filename.split('_')[-1]
+
+        timestamps_dir = './output/TIMESTAMPS'
+        fps_dir = './output/FPS'
+
+        timestamps_dir_exists = os.path.isdir(timestamps_dir)
+        fps_dir_exists = os.path.isdir(fps_dir)
+
+        if not timestamps_dir_exists:
+            os.mkdir(timestamps_dir)
+
+        if not fps_dir_exists:
+            os.mkdir(fps_dir)
+
+        if (type == "ms"):
+            shutil.copy(filepath, timestamps_dir)
+            os.remove(filepath)
+
+        elif (type == "fps"):
+            shutil.copy(filepath, fps_dir)
+            os.remove(filepath)
+
+    print("RGB .npy files have been organised.")
+
 
 if __name__ == '__main__':
     args_cli = OmegaConf.from_cli()
